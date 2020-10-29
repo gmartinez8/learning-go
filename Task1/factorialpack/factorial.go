b2bb04864acf234411b4a7e7839ad63e2786b11e
Gustavo <gustavomartinez1990@gmail.com>
2020-10-29 07:52:12 -0400

    Update package

--- Task1/factorialpack/factorial.go
+++ Task1/factorialpack/factorial.go
@@ -1,6 +1,6 @@
 package factorialpack
 
-//First letter in function Factorial should be uppercase for function to be exported
+//Factorial First letter in function  should be uppercase for function to be exported
 func Factorial(number uint64) uint64 {
 	if number == 0 {
 		return 1
